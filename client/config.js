02a6382ccd232864cd83ceea39272dc27d0bc44b
m3rri1 <hellomerril@gmail.com>
2026-02-28 08:35:55 +0530

    add config

--- client/config.js
+++ client/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  API: 'http://192.168.1.5:3000/api',
-  SOCKET: 'http://192.168.1.5:3000'
+  API: 'https://restaurant-system-cfma.onrender.com/api',
+  SOCKET: 'https://restaurant-system-cfma.onrender.com'
 };
